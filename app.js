9b33fea9785eb238e5db82d390943687de1e070d
Gontar Daniil <gontardaniil@gmail.com>
2023-12-12 20:35:04 +0200

    change function 'app.get'

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const app = express();
 const router = require('./routes/views');
@@ -5,6 +6,19 @@ const router = require('./routes/views');
 app.use(router);
 
 const PORT = process.env.PORT || 10000;
+
+app.get('/', (req, res) => {
+  const currentDate = new Date();
+  const status = 'success';
+
+  const response = {
+    date: currentDate,
+    status: status,
+    message: 'hello from back-end', 
+  };
+
+  res.status(200).json(response);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
