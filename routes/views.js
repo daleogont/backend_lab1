bed301b66c0246d05780b1b5455c5c0ebe454771
Gontar Daniil <gontardaniil@gmail.com>
2023-12-26 21:23:42 +0200

    Add endpoints for wallet

--- routes/views.js
+++ routes/views.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const {User} = require('../models/index');
 const {Category} = require('../models/index');
 const {Record} = require('../models/index');
+const {Wallet} = require('../models/index');
 
 router.post('/user', async (req, res) => {
   const { user_name } = req.body;
@@ -93,7 +94,7 @@ router.post('/category', async (req, res) => {
     const validationResult = categoryPostSchema.validate({ cat_name });
 
     if (validationResult.error) {
-      return res.status(400).json({ error: validationResult.error.details[0].message });
+      return res.status(400).json({message: validationResult.error.details[0].message });
   }
 
     
@@ -104,7 +105,7 @@ router.post('/category', async (req, res) => {
     res.status(201).json(category);
   } catch (error) {
     console.error('Failure creating category:', error);
-    res.status(500).json({ error: 'Server Malfunction' });
+    res.status(500).json({message: 'Server Malfunction' });
   }
 });
 
@@ -116,7 +117,7 @@ router.get('/categories', async (req, res) => {
     res.status(200).json(categories);
   } catch (error) {
     console.error('Failure fetching categories:', error);
-    res.status(500).json({ error: 'Server Malfunction' });
+    res.status(500).json({message: 'Server Malfunction' });
   }
 });
 
@@ -126,20 +127,20 @@ router.get('/category/:cat_id', async (req, res) => {
   const validationResult = categoryGetSchema.validate({ cat_id });
 
     if (validationResult.error) {
-      return res.status(400).json({ error: validationResult.error.details[0].message });
+      return res.status(400).json({message: validationResult.error.details[0].message });
   }
 
   try {
     const curCat = await Category.findByPk(cId);
 
     if (!curCat) {
-      return res.status(404).json({ error: 'Invalid cat_id' });
+      return res.status(404).json({message: 'Invalid cat_id' });
     }
 
     res.status(200).json(curCat);
   } catch (error) {
     console.error(`Error fetching category with cat_id ${cId}:`, error);
-    res.status(500).json({ error: 'Server Malfunction' });
+    res.status(500).json({message: 'Server Malfunction' });
   }
 });
 
@@ -149,14 +150,14 @@ router.delete('/category/:cat_id', async (req, res) => {
   const validationResult = categoryGetSchema.validate({ cat_id });
 
     if (validationResult.error) {
-      return res.status(400).json({ error: validationResult.error.details[0].message });
+      return res.status(400).json({ message: validationResult.error.details[0].message });
   }
 
   try {
     const deletedCategory = await Category.findByPk(cId);
 
     if (!deletedCategory) {
-      return res.status(404).json({ error: 'Invalid cat_id' });
+      return res.status(404).json({message: 'Invalid cat_id' });
     }
 
     await deletedCategory.destroy();
@@ -164,7 +165,7 @@ router.delete('/category/:cat_id', async (req, res) => {
     res.status(200).json(deletedCategory);
   } catch (error) {
     console.error(`Failure deleting category with cat_id ${cId}:`, error);
-    res.status(500).json({ error: 'Server Malfunction' });
+    res.status(500).json({message: 'Server Malfunction' });
   }
 });
 
@@ -176,7 +177,7 @@ router.post('/record', async (req, res) => {
   const validationResult = recordSchema.validate({ user_id, cat_id, amount });
 
   if (validationResult.error) {
-    return res.status(400).json({ error: validationResult.error.details[0].message });
+    return res.status(400).json({message: validationResult.error.details[0].message });
   }
 
   try {
@@ -184,7 +185,7 @@ router.post('/record', async (req, res) => {
     const category = await Category.findByPk(cat_id);
 
     if (!user || !category) {
-      return res.status(400).json({ error: 'Invalid input' });
+      return res.status(400).json({message: 'Invalid input' });
     }
 
     const record = await Record.create({
@@ -198,7 +199,7 @@ router.post('/record', async (req, res) => {
     });
 
     if (!wallet) {
-      return res.status(400).json({ error: 'Wallet not found' });
+      return res.status(400).json({message: 'Wallet not found' });
     }
 
     wallet.balance -= amount;
@@ -207,7 +208,7 @@ router.post('/record', async (req, res) => {
     res.status(201).json(record);
   } catch (error) {
     console.error('Failure creating record:', error);
-    res.status(500).json({ error: 'Server Malfunction' });
+    res.status(500).json({message: 'Server Malfunction' });
   }
 });
 
@@ -220,7 +221,7 @@ router.get('/records', async (req, res) => {
     res.status(200).json(records);
   } catch (error) {
     console.error('Error fetching categories:', error);
-    res.status(500).json({ error: 'Server Malfunction' });
+    res.status(500).json({message: 'Server Malfunction' });
   }
 });
 
@@ -244,7 +245,7 @@ router.get('/record', async (req, res) => {
     res.status(200).json(filteredRecords);
   } catch (error) {
     console.error('Error fetching records:', error);
-    res.status(500).json({ error: 'Server Malfunction' });
+    res.status(500).json({message: 'Server Malfunction' });
   }
 });
 
@@ -256,7 +257,7 @@ router.get('/record', async (req, res) => {
       const deletedRecord = await Record.findByPk(rec_id);
   
       if (!deletedRecord) {
-        return res.status(404).json({ error: 'Invalid rec_id' });
+        return res.status(404).json({message: 'Invalid rec_id' });
       }
   
       await deletedRecord.destroy();
@@ -264,11 +265,134 @@ router.get('/record', async (req, res) => {
       res.status(200).json(deletedRecord);
     } catch (error) {
       console.error(`Error deleting record with rec_id ${rec}:`, error);
-      res.status(500).json({ error: 'Server Malfunction' });
+      res.status(500).json({message: 'Server Malfunction' });
     }
   });
 
 //>>>>>>>>>>>>>>>>End>>>>>>>>>>>>>>>//
+router.post('/wallet', async (req, res) => {
+  const { user_id } = req.body;
+
+  const validationResult = walletPostSchema.validate({ user_id });
+
+    if (validationResult.error) {
+      return res.status(400).json({message: validationResult.error.details[0].message });
+    }
+
+  try {
+    
+    const user = await User.findByPk(user_id);
+
+    if (!user) {
+      return res.status(400).json({message: 'User not found' });
+    }
+
+    const existingWallet = await Wallet.findOne({
+      where: { user_id },
+    });
+
+    if (existingWallet) {
+      return res.status(400).json({message: 'Wallet already exists for this user' });
+    }
+
+    const wallet = await Wallet.create({
+      user_id,
+      balance: 0,
+    });
+
+    res.status(201).json(wallet);
+  } catch (error) {
+    console.error('Error creating wallet:', error);
+    res.status(500).json({message: 'Server Malfunction' });
+  }
+});
+
+router.post('/raise/:user_id', async (req, res) => {
+  const { user_id } = req.params;
+  const { amount } = req.body;
+
+  const validationResult = walletRaiseSchema.validate({ user_id, amount });
+
+    if (validationResult.error) {
+      return res.status(400).json({message: validationResult.error.details[0].message });
+    }
+
+  try {
+    let wallet = await Wallet.findOne({ where: { user_id } });
+
+    if (!wallet) {
+      wallet = await Wallet.create({ user_id, balance: 0 });
+    }
+
+    wallet.balance += amount;
+    await wallet.save();
+
+    res.status(200).json({ user_id, new_balance: wallet.balance });
+  } catch (error) {
+    console.error(`Error adding amount to balance for user_id ${user_id}:`, error);
+    res.status(500).json({message: 'Server Malfunction' });
+  }
+});
+
+router.get('/wallets', async (req, res) => {
+  
+  try {
+    const wallets = await Wallet.findAll();
+
+    res.status(200).json(wallets);
+  } catch (error) {
+    console.error('Error fetching categories:', error);
+    res.status(500).json({message: 'Server Malfunction' });
+  }
+});
+
+router.get('/wallet/:user_id', async (req, res) => {
+  const { user_id } = req.params;
+
+  const validationResult = walletGetSchema.validate({ user_id});
+
+    if (validationResult.error) {
+      return res.status(400).json({message: validationResult.error.details[0].message });
+    }
+
+  try {
+    const wallet = await Wallet.findOne({ where: { user_id } });
+
+    if (!wallet) {
+      return res.status(404).json({message: 'No wallet found for user_id' });
+    }
+
+    res.status(200).json({ user_id, balance: wallet.balance });
+  } catch (error) {
+    console.error(`Error fetching balance for user_id ${user_id}:`, error);
+    res.status(500).json({message: 'Server Malfunction' });
+  }
+});
+
+router.delete('/wallet/:user_id', async (req, res) => {
+  const { user_id } = req.params;
+
+  const validationResult = walletGetSchema.validate({ user_id});
+
+    if (validationResult.error) {
+      return res.status(400).json({message: validationResult.error.details[0].message });
+    }
+
+  try {
+    const deletedWallet = await Wallet.findOne({ where: { user_id } });
+
+    if (!deletedWallet) {
+      return res.status(404).json({message: 'No wallet found for user_id' });
+    }
+
+    await deletedWallet.destroy();
+
+    res.status(200).json({ user_id, message: 'Wallet deleted successfully' });
+  } catch (error) {
+    console.error(`Error deleting wallet for user_id ${user_id}:`, error);
+    res.status(500).json({message: 'Server Malfunction' });
+  }
+});
 
 //>>>>>>>>>>>>>>>>>>2>>>>>>>>>>>>>//
 router.get('/healthcheck', (req, res) => {
